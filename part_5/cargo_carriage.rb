bdd6aef4757dc4a85dbd0617d9540024915c3ae1
dendomenko <dendomenko@gmail.com>
2018-02-07 15:23:50 +0200

    Add volume and methods to work with to cargo carriage

--- part_5/cargo_carriage.rb
+++ part_5/cargo_carriage.rb
@@ -1,5 +1,16 @@
 class CargoCarriage < Carriage
-  def initialize
+  attr_reader :occupied_volume
+  def initialize(volume)
+    @volume = volume
+    @occupied_volume = 0
     @type = 'cargo'
+  end
+
+  def occupy_volume(volume)
+    @occupied_volume += volume if volume <= free_volume
+  end
+
+  def free_volume
+    @volume - occupied_volume
   end
 end
