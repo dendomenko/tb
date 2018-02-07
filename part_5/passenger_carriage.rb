6177fbeedab6c5e3ad8c8da1fbf29d655f4bbcb0
dendomenko <dendomenko@gmail.com>
2018-02-07 15:14:39 +0200

    Add seats to passenger carriage

--- part_5/passenger_carriage.rb
+++ part_5/passenger_carriage.rb
@@ -1,6 +1,16 @@
 class PassengerCarriage < Carriage
+  attr_reader :taken_seats
   def initialize(seats)
     @seats = seats
     @type = 'passenger'
+    @taken_seats = 0
+  end
+
+  def take_seat
+    @taken_seats += 1 unless @take_seats == @seats
+  end
+
+  def free_seats
+    @seats - @taken_seats
   end
 end
